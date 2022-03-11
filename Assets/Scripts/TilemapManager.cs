b30fdb608770cfa5161d7645ea7fe00254a08f8c
Aaron <aaron.golder@gmx.de>
2022-03-11 16:21:14 +0100

    Add: Funktionalität zu Toggle Knopf hinzugefügt
    Funktionalität zu Toggle Knopf hinzugefügt.

--- Assets/Scripts/TilemapManager.cs
+++ Assets/Scripts/TilemapManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using TMPro;
+using UnityEngine.UI;
 
 public class TilemapManager : MonoBehaviour
 {
@@ -9,10 +11,14 @@ public class TilemapManager : MonoBehaviour
     private Tilemap tilemap;
     [SerializeField]
     private TileBase grasstile;
+    [SerializeField]
+    private Toggle Debug_InputTileToggle;
+    [SerializeField]
+    private TMP_InputField Debug_InputTileID;
 
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0) && Debug_InputTileToggle.isOn)
         {
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector3Int gridPos = tilemap.WorldToCell(mousePos);
